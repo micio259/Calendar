2540c810293e2b4b260ee6db72b8d98d4a0491a4
Michal Wziatek <greenandroiduser@gmail.com>
2019-08-25 16:25:53 +0200

    Initial Commit

---
+++ Calendar/PageViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Michał Wziątek on 25/08/2019.
+//  Copyright © 2019 Michał Wziątek. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor.green
+        
+    }
+
+
+}
+
